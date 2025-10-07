6649f2c056f7e0e6d9a31cce85348cbdbb408905
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2025-10-07 12:31:34 +0300

    Теперь fake файлы качаются одним архивом. Это куда быстрее при установке и обновлении и меньше шанса зависнуть на wget

--- z4r.sh
+++ z4r.sh
@@ -54,7 +54,9 @@ backup_strats() {
 get_repo() {
  mkdir -p /opt/zapret/lists /opt/zapret/extra_strats/TCP/{RKN,User,YT,temp} /opt/zapret/extra_strats/UDP/YT
  for listfile in autohostlist.txt cloudflare-ipset.txt cloudflare-ipset_v6.txt mycdnlist.txt myhostlist.txt netrogat.txt russia-discord.txt russia-youtube-rtmps.txt russia-youtube.txt russia-youtubeQ.txt; do wget -4 -P /opt/zapret/lists https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/master/lists/$listfile; done
- for fakefile in http_fake_MS.bin quic_{1..7}.bin syn_packet.bin tls_clienthello_{1..18}.bin tls_clienthello_2n.bin tls_clienthello_6a.bin; do wget -4 -P /opt/zapret/files/fake/ https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/master/fake/$fakefile; done
+ wget -4 -O /opt/zapret/files/fake/fake_files.tar.gz "https://github.com/IndeecFOX/zapret4rocket/raw/master/fake_files.tar.gz"
+ tar -xzf /opt/zapret/files/fake/fake_files.tar.gz -C /opt/zapret/files/fake
+ rm -f /opt/zapret/files/fake/fake_files.tar.gz 
  wget -4 -O /opt/zapret/extra_strats/UDP/YT/List.txt https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/master/extra_strats/UDP/YT/List.txt
  wget -4 -O /opt/zapret/extra_strats/TCP/RKN/List.txt https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/master/extra_strats/TCP/RKN/List.txt
  wget -4 -O /opt/zapret/extra_strats/TCP/YT/List.txt https://raw.githubusercontent.com/IndeecFOX/zapret4rocket/master/extra_strats/TCP/YT/List.txt
