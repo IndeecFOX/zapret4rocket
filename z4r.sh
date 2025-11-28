dbb712578f7a66e439b64d5cc5916a2f5445f679
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2025-11-28 21:19:56 +0300

    add supporter info I_ZNA_I

--- z4r.sh
+++ z4r.sh
@@ -665,7 +665,7 @@ Enter (без цифр) - переустановка/обновление zapret
    exit 7
    ;;
   "777")
-   echo -e "${green}Специальный zeefeer premium для Valery ProD, Xoz, Andrei_5288515371, Dina_turat, Александра, АлександраП, vecheromholodno, Евгения Головащенко, Dyadyabo, skuwakin, izzzgoy, Grigaraz и subzeero452 активирован. Наверное. Так же благодарю поддержавших проект comandante1928, VssA, vladdrazz, Alexey_Tob, Bor1sBr1tva, porfenon123, DA00001, geodomin и анонимов${plain}"
+   echo -e "${green}Специальный zeefeer premium для Valery ProD, Xoz, Andrei_5288515371, Dina_turat, Александра, АлександраП, vecheromholodno, Евгения Головащенко, Dyadyabo, skuwakin, izzzgoy, Grigaraz и subzeero452 активирован. Наверное. Так же благодарю поддержавших проект comandante1928, VssA, vladdrazz, Alexey_Tob, Bor1sBr1tva, porfenon123, DA00001, geodomin, I_ZNA_I и анонимов${plain}"
    exit_to_menu
    ;;
   esac
