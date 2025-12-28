a7eaba694e6bdcd6cd03ed65b4bfdb614af6a86b
IndeecFOX <44396407+IndeecFOX@users.noreply.github.com>
2025-12-28 19:19:10 +0300

    Фикс ошибки, когда при переходе к номерной стратегии прошлые не переставали работать

--- z4r.sh
+++ z4r.sh
@@ -147,6 +147,9 @@ try_strategies() {
 		echo "Введено значение не из диапазона 1-17. Начинаем с 1 стратегии"
 		strat_num=1
 	fi
+	for ((clr_txt=1; clr_txt<=count; clr_txt++)); do
+		: > "$base_path/$clr_txt.txt"
+	done
     for ((strat_num=strat_num; strat_num<=count; strat_num++)); do
         if [[ $strat_num -ge 2 ]]; then
             prev=$((strat_num - 1))
