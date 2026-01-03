fd7bc796006a942d655ca8f983110e2a565ce92c
AloofLibra <87386304+AloofLibra@users.noreply.github.com>
2026-01-03 04:04:31 +0300

    Update z4r.sh

--- z4r.sh
+++ z4r.sh
@@ -71,6 +71,17 @@ source "$SCRIPT_DIR/lib/submenus.sh"
 #          menu_action_toggle_fwtype, menu_action_toggle_udp_range
 source "$SCRIPT_DIR/lib/actions.sh" 
 
+change_user() {
+   if /opt/zapret/nfq/nfqws --dry-run --user="nobody" 2>&1 | grep -q "queue"; then
+    echo "WS_USER=nobody"
+	sed -i 's/^#\(WS_USER=nobody\)/\1/' /opt/zapret/config.default
+   elif /opt/zapret/nfq/nfqws --dry-run --user="$(head -n1 /etc/passwd | cut -d: -f1)" 2>&1 | grep -q "queue"; then
+    echo "WS_USER=$(head -n1 /etc/passwd | cut -d: -f1)"
+    sed -i "s/^#WS_USER=nobody$/WS_USER=$(head -n1 /etc/passwd | cut -d: -f1)/" "/opt/zapret/config.default"
+   else
+    echo -e "${yellow}WS_USER не подошёл. Скорее всего будут проблемы. Если что - пишите в саппорт${plain}"
+   fi
+}
 
 #Создаём папки и забираем файлы папок lists, fake, extra_strats, копируем конфиг, скрипты для войсов DS, WA, TG
 get_repo() {
