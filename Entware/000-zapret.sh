be2302da333a2341b3c0c0cad051d8dde982024e
AloofLibra <87386304+AloofLibra@users.noreply.github.com>
2026-01-07 19:44:28 +0300

    Ещё немного фиксов путей, часть 4

--- Entware/000-zapret.sh
+++ Entware/000-zapret.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 [ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit 0
-ZAPRET2_INIT="/opt/zapret2/init.d/sysv/zapret2"
 if [ -f "/opt/zapret2/init.d/openwrt/zapret2" ]; then
   ZAPRET2_INIT="/opt/zapret2/init.d/openwrt/zapret2"
+elif [ -f "/opt/zapret2/init.d/sysv/zapret2" ]; then
+  ZAPRET2_INIT="/opt/zapret2/init.d/sysv/zapret2"
+else
+  ZAPRET2_INIT="/opt/zapret2/init.d/sysv/zapret2"
 fi
 "$ZAPRET2_INIT" restart-fw
 exit 0
