3ae7a8291acb3c78ef0bfd1a524770cae1579af3
AloofLibra <87386304+AloofLibra@users.noreply.github.com>
2026-01-07 19:43:14 +0300

    Ещё немного фиксов путей, часть 2

--- merlin_wan_restart_zapret.sh
+++ merlin_wan_restart_zapret.sh
@@ -9,9 +9,12 @@ then
     wan_logger "Waiting 5000ms after connected"
     sleep 5
     wan_logger "Restarting zapret"
-    ZAPRET2_INIT="/opt/zapret2/init.d/sysv/zapret2"
     if [ -f "/opt/zapret2/init.d/openwrt/zapret2" ]; then
       ZAPRET2_INIT="/opt/zapret2/init.d/openwrt/zapret2"
+    elif [ -f "/opt/zapret2/init.d/sysv/zapret2" ]; then
+      ZAPRET2_INIT="/opt/zapret2/init.d/sysv/zapret2"
+    else
+      ZAPRET2_INIT="/opt/zapret2/init.d/sysv/zapret2"
     fi
     "$ZAPRET2_INIT" restart
     wan_logger "Zapret restarted"
